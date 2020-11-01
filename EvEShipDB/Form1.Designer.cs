c395ad914d2448a08d2a8b7c7e732bd01d7273ca
Tristan Dubé-Lepage <tristan.dube@gmail.com>
2020-11-01 15:05:07 -0500

    Initial commit
    Prototype implementation.

---
+++ EvEShipDB/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace EvEShipDB
+{
+    partial class databaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shipsList = new System.Windows.Forms.DataGridView();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.nameEditBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.classEditBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.techEditBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.shipsList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shipsList
+            // 
+            this.shipsList.AllowUserToAddRows = false;
+            this.shipsList.AllowUserToDeleteRows = false;
+            this.shipsList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.shipsList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.shipsList.Location = new System.Drawing.Point(12, 12);
+            this.shipsList.Name = "shipsList";
+            this.shipsList.ReadOnly = true;
+            this.shipsList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.shipsList.Size = new System.Drawing.Size(930, 150);
+            this.shipsList.TabIndex = 0;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.Location = new System.Drawing.Point(12, 340);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(930, 150);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // nameEditBox
+            // 
+            this.nameEditBox.Location = new System.Drawing.Point(53, 173);
+            this.nameEditBox.Name = "nameEditBox";
+            this.nameEditBox.Size = new System.Drawing.Size(100, 20);
+            this.nameEditBox.TabIndex = 2;
+            this.nameEditBox.TextChanged += new System.EventHandler(this.nameEditBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Class";
+            // 
+            // classEditBox
+            // 
+            this.classEditBox.Location = new System.Drawing.Point(195, 172);
+            this.classEditBox.Name = "classEditBox";
+            this.classEditBox.Size = new System.Drawing.Size(100, 20);
+            this.classEditBox.TabIndex = 4;
+            this.classEditBox.TextChanged += new System.EventHandler(this.classEditBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tech";
+            // 
+            // techEditBox
+            // 
+            this.techEditBox.Location = new System.Drawing.Point(341, 173);
+            this.techEditBox.Name = "techEditBox";
+            this.techEditBox.Size = new System.Drawing.Size(100, 20);
+            this.techEditBox.TabIndex = 6;
+            this.techEditBox.TextChanged += new System.EventHandler(this.techEditBox_TextChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(450, 170);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(531, 170);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 9;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadButton.Location = new System.Drawing.Point(867, 168);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 10;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(867, 197);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(612, 170);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // databaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 502);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.techEditBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.classEditBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameEditBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.shipsList);
+            this.Name = "databaseForm";
+            this.Text = "EvE Ships Database";
+            ((System.ComponentModel.ISupportInitialize)(this.shipsList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView shipsList;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox nameEditBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox classEditBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox techEditBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ EvEShipDB/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net.Http.Headers;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Runtime.DesignerServices;
+
+namespace EvEShipDB
+{
+    public partial class databaseForm : Form
+    {
+        DataTable dataSource;
+        string inputFilePath = "";
+
+        public databaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+            if(this.shipsList.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Select a row first.");
+                return;
+            }
+
+            DataGridViewRow selectedElement = this.shipsList.SelectedRows[0];
+            
+
+            string newText = inputBox.Text;
+
+            string[] attributes = newText.Split('\n');
+
+            foreach (string item in attributes)
+            {
+                string[] attr = item.Split('\t');
+
+                if(attr.Length == 1) // Group attribute, no data
+                {
+                    continue;
+                }
+
+                string name = attr[0].Replace(",", "");
+                string value = attr[1].Replace(",", "");
+
+                setAttributeInGrid(name, value, selectedElement);
+            }
+        }
+
+        private void setAttributeInGrid(string name, string value, DataGridViewRow row)
+        {
+            foreach (DataGridViewCell cell in row.Cells)
+            {
+                if(cell.OwningColumn.Name == name)
+                {
+                    cell.Value = value;
+                }
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            diag.Filter = "CSV Files|*.csv";
+            var result = diag.ShowDialog();
+
+            if(result != DialogResult.OK)
+                return;
+
+            inputFilePath = diag.FileName;
+
+            this.dataSource = new DataTable();
+
+            string[] data = File.ReadAllLines(inputFilePath);
+            string[] headers = data[0].Split(',');
+
+            foreach (string header in headers)
+            {
+                dataSource.Columns.Add(header.Replace(",", ""));
+            }
+
+            foreach (string lines in data.Skip(1))
+            {
+                //string[] attributes = lines.Split(',');
+                string[] attributes = lines.Split(',').Select(p => p.Replace(",", "")).ToArray();
+
+                dataSource.Rows.Add(attributes);
+            }
+
+            this.shipsList.DataSource = dataSource;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = getCurrentRow();
+            if(row != null)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Value = "";
+                }
+            }
+        }
+
+        private DataGridViewRow getCurrentRow()
+        {
+            if (this.shipsList.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Select a row first.");
+                
+                return null;
+            }
+
+            return this.shipsList.SelectedRows[0];
+        }
+
+        private void nameEditBox_TextChanged(object sender, EventArgs e)
+        {
+            DataGridViewRow row = getCurrentRow();
+            if (row != null)
+            {
+                setAttributeInGrid("Name", nameEditBox.Text, row);
+            }
+        }
+
+        private void classEditBox_TextChanged(object sender, EventArgs e)
+        {
+            DataGridViewRow row = getCurrentRow();
+            if (row != null)
+            {
+                setAttributeInGrid("Class", classEditBox.Text, row);
+            }
+
+        }
+
+        private void techEditBox_TextChanged(object sender, EventArgs e)
+        {
+            DataGridViewRow row = getCurrentRow();
+            if (row != null)
+            {
+                setAttributeInGrid("Tech", techEditBox.Text, row);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter outFile = File.CreateText(inputFilePath);
+            CsvWriter writer = new CsvWriter(outFile, CultureInfo.InvariantCulture);
+
+            foreach (DataColumn item in this.dataSource.Columns)
+            {
+                writer.WriteField(item.ColumnName);
+            }
+            writer.NextRecord();
+
+            foreach (DataRow item in this.dataSource.Rows)
+            {
+                List<string> values = item.ItemArray.Cast<Object>().Select(p =>
+                {
+                    if (p.GetType() == typeof(System.DBNull))
+                    {
+                        return "";
+                    }
+
+                    return p;
+                }).Cast<string>().ToList();
+
+                foreach (string attribute in values)
+                {
+                    writer.WriteField(attribute);
+                }
+
+                writer.NextRecord();
+            }
+
+            writer.Dispose();
+            outFile.Dispose();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            this.dataSource.Rows.Add();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = getCurrentRow();
+            if(row != null)
+            {
+                this.dataSource.Rows.RemoveAt(row.Index);
+            }
+        }
+    }
+}
